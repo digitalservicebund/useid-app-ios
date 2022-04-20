b6be979422dcd467df5edec95336e54cf7320a33
Fabio Tacke <fabio@tacke.berlin>
2022-04-20 15:25:11 +0200

    Initial Commit

---
+++ BundID/BundIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundIDApp.swift
+//  BundID
+//
+//  Created by Fabio Tacke on 20.04.22.
+//
+
+import SwiftUI
+
+@main
+struct BundIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BundID/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BundID
+//
+//  Created by Fabio Tacke on 20.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
