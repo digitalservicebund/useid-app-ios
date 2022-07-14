bdb0a57dfa3cbc0959ffe18bc709e678cb485985
Andreas Ganske <a.ganske@chaosspace.de>
2022-07-14 16:22:52 +0200

    Fix release build and add codesigning

--- BundID/Helpers/Store+Empty.swift
+++ BundID/Helpers/Store+Empty.swift
@@ -1,8 +1,10 @@
 import Foundation
 import ComposableArchitecture
 
+#if PREVIEW
 extension Store where State == Void {
     static var empty: Store {
         Store<State, Action>.init(initialState: (), reducer: .empty, environment: AppEnvironment.preview)
     }
 }
+#endif
