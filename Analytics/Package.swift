3a1314f9858f613198bd14437a85c0aaede99744
Tobias Ottenweller <tobias.ottenweller@digitalservice.bund.de>
2022-08-30 17:01:20 +0200

    Set correct swit-tools-version and deployment target for Analytics

--- Analytics/Package.swift
+++ Analytics/Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Analytics",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Analytics",
