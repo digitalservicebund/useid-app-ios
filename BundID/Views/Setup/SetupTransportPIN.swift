1141370fefb303b3ac776b8367c15d82ca765361
Tobias Ottenweller <tobias.ottenweller@digitalservice.bund.de>
2022-09-02 11:10:32 +0200

    Add missing body for transport pin entry

--- BundID/Views/Setup/SetupTransportPIN.swift
+++ BundID/Views/Setup/SetupTransportPIN.swift
@@ -20,7 +20,7 @@ let setupTransportPINReducer = Reducer<SetupTransportPINState, SetupTransportPIN
 
 struct SetupTransportPIN: View {
     
-    var store: Store<SetupTransportPINState, SetupTransportPINAction>
+    let store: Store<SetupTransportPINState, SetupTransportPINAction>
     @State var digits = 5
     
     init(store: Store<SetupTransportPINState, SetupTransportPINAction>) {
@@ -28,40 +28,42 @@ struct SetupTransportPIN: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 24) {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 24) {
+                Text(L10n.FirstTimeUser.TransportPIN.title)
+                    .font(.bundLargeTitle)
+                    .foregroundColor(.blackish)
+                Text(L10n.FirstTimeUser.TransportPIN.body)
+                    .font(.bundBody)
+                    .foregroundColor(.blackish)
+                ZStack {
+                    Asset.transportPIN.swiftUIImage
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
                     WithViewStore(store) { viewStore in
-                        Text(L10n.FirstTimeUser.TransportPIN.title)
-                            .font(.bundLargeTitle)
-                            .foregroundColor(.blackish)
-                        ZStack {
-                            Image(decorative: "Transport-PIN")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            PINEntryView(pin: viewStore.binding(\.$enteredPIN),
-                                         maxDigits: digits,
-                                         label: L10n.FirstTimeUser.TransportPIN.textFieldLabel,
-                                         shouldBeFocused: viewStore.binding(\.$focusTextField),
-                                         doneConfiguration: DoneConfiguration(enabled: viewStore.enteredPIN.count == digits,
-                                                                              title: L10n.FirstTimeUser.TransportPIN.continue,
-                                                                              handler: { pin in
-                                    viewStore.send(.done(transportPIN: pin))
-                            }))
-                            .font(.bundTitle)
-                            .background(
-                                Color.white.cornerRadius(10)
-                            )
-                            .padding(40)
-                            // Focus: iOS 15 only
-                            // Done button above keyboard: iOS 15 only
-                        }
+                        PINEntryView(pin: viewStore.binding(\.$enteredPIN),
+                                     maxDigits: digits,
+                                     label: L10n.FirstTimeUser.TransportPIN.textFieldLabel,
+                                     shouldBeFocused: viewStore.binding(\.$focusTextField),
+                                     doneConfiguration: DoneConfiguration(enabled: viewStore.enteredPIN.count == digits,
+                                                                          title: L10n.FirstTimeUser.TransportPIN.continue,
+                                                                          handler: { pin in
+                            viewStore.send(.done(transportPIN: pin))
+                        }))
+                        .font(.bundTitle)
+                        .background(Color.white.cornerRadius(10))
+                        .padding(40)
+                        // Focus: iOS 15 only
+                        // Done button above keyboard: iOS 15 only
                     }
                 }
-                .padding(.horizontal)
+                
             }
+            .padding(.horizontal)
         }
         .navigationBarTitleDisplayMode(.inline)
+#if PREVIEW
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button("\(Image(systemName: "arrow.left.and.right")) \(digits == 5 ? "6" : "5")") {
@@ -69,6 +71,7 @@ struct SetupTransportPIN: View {
                 }
             }
         }
+#endif
     }
 }
 
@@ -76,14 +79,14 @@ struct SetupTransportPIN_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             SetupTransportPIN(store: Store(initialState: .init(),
-                                                         reducer: .empty,
-                                                         environment: AppEnvironment.preview))
+                                           reducer: .empty,
+                                           environment: AppEnvironment.preview))
         }
         .previewDevice("iPhone SE (2nd generation)")
         NavigationView {
             SetupTransportPIN(store: Store(initialState: .init(enteredPIN: "12345"),
-                                                         reducer: .empty,
-                                                         environment: AppEnvironment.preview))
+                                           reducer: .empty,
+                                           environment: AppEnvironment.preview))
         }
         .previewDevice("iPhone SE (2nd generation)")
     }
