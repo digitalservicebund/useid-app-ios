9697a829644968841a6c009895fd87f50db9586e
Andreas Ganske <a.ganske@chaosspace.de>
2022-09-22 23:05:31 +0200

    Add a switch to 6-digit pin to incorrect transport pin screen

--- BundID/Views/Setup/SetupIncorrectTransportPIN.swift
+++ BundID/Views/Setup/SetupIncorrectTransportPIN.swift
@@ -7,6 +7,12 @@ import IdentifiedCollections
 struct SetupIncorrectTransportPINState: Equatable {
     var remainingAttempts: Int
     
+#if DEBUG && !PREVIEW
+    var maxDigits: Int = 5
+#else
+    var maxDigits: Int { 5 }
+#endif
+    
     @BindableState var enteredPIN: String = ""
     @BindableState var focusTextField: Bool = true
     @BindableState var alert: AlertState<SetupIncorrectTransportPINAction>?
@@ -18,6 +24,10 @@ enum SetupIncorrectTransportPINAction: BindableAction, Equatable {
     case confirmEnd
     case dismissAlert
     case binding(BindingAction<SetupIncorrectTransportPINState>)
+    
+#if DEBUG && !PREVIEW
+    case toggleDigitCount
+#endif
 }
 
 let setupIncorrectTransportPINReducer = Reducer<SetupIncorrectTransportPINState, SetupIncorrectTransportPINAction, AppEnvironment> { state, action, _ in
@@ -29,6 +39,15 @@ let setupIncorrectTransportPINReducer = Reducer<SetupIncorrectTransportPINState,
                                                              action: .send(.confirmEnd)),
                                  secondaryButton: .cancel(TextState(verbatim: L10n.FirstTimeUser.ConfirmEnd.deny)))
         return .none
+#if DEBUG && !PREVIEW
+    case .toggleDigitCount:
+        if state.maxDigits == 5 {
+            state.maxDigits = 6
+        } else {
+            state.maxDigits = 5
+        }
+        return .none
+#endif
     default:
         return .none
     }
@@ -55,18 +74,16 @@ struct SetupIncorrectTransportPIN: View {
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                 PINEntryView(pin: viewStore.binding(\.$enteredPIN),
-                                             maxDigits: 5,
+                                             maxDigits: viewStore.maxDigits,
                                              label: L10n.FirstTimeUser.IncorrectTransportPIN.textFieldLabel,
                                              shouldBeFocused: viewStore.binding(\.$focusTextField),
-                                             doneConfiguration: DoneConfiguration(enabled: viewStore.enteredPIN.count == 5,
+                                             doneConfiguration: DoneConfiguration(enabled: viewStore.enteredPIN.count == viewStore.maxDigits,
                                                                                   title: L10n.FirstTimeUser.IncorrectTransportPIN.continue,
                                                                                   handler: { pin in
                                     viewStore.send(.done(transportPIN: pin))
                                 }))
                                 .font(.bundTitle)
-                                .background(
-                                    Color.white.cornerRadius(10)
-                                )
+                                .background(Color.white.cornerRadius(10))
                                 .padding(40)
                             }
                             VStack(spacing: 24) {
@@ -95,6 +112,17 @@ struct SetupIncorrectTransportPIN: View {
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarHidden(false)
+#if DEBUG && !PREVIEW
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    WithViewStore(store) { viewStore in
+                        Button("\(Image(systemName: "arrow.left.and.right")) \(viewStore.maxDigits == 5 ? "6" : "5")") {
+                            viewStore.send(.toggleDigitCount)
+                        }
+                    }
+                }
+            }
+#endif
             .alert(store.scope(state: \.alert), dismiss: .dismissAlert)
         }
         .interactiveDismissDisabled {
