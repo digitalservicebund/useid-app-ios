ae544420b6c9558cf3487b6e14e6c0a9fea4e045
Andreas Ganske <a.ganske@chaosspace.de>
2022-12-02 12:12:04 +0100

    Define styles for box

--- BundesIdent/Views/SupplementaryViews/Box.swift
+++ BundesIdent/Views/SupplementaryViews/Box.swift
@@ -3,19 +3,34 @@ import SwiftUI
 struct BoxContent: Hashable {
     enum Style: Hashable {
         case info
+        case warning
         case error
+        case success
         
         var backgroundColor: Color {
             switch self {
-            case .info: return .yellow200
+            case .info: return .blue200
+            case .warning: return .yellow200
             case .error: return .red200
+            case .success: return .green100
             }
         }
         
         var iconColor: Color {
             switch self {
-            case .info: return .blackish
+            case .info: return .blue700
+            case .warning: return .orange400
             case .error: return .red900
+            case .success: return .green800
+            }
+        }
+        
+        var iconSystemName: String {
+            switch self {
+            case .info: return "info.circle.fill"
+            case .warning: return "exclamationmark.triangle.fill"
+            case .error: return "xmark.circle.fill"
+            case .success: return "checkmark.circle.fill"
             }
         }
     }
@@ -29,21 +44,18 @@ struct Box: View {
     let content: BoxContent
     
     var body: some View {
-        VStack(spacing: 8) {
-            HStack {
-                Image(systemName: "exclamationmark.circle")
-                    .foregroundColor(content.style.iconColor)
-                    .accessibilityHidden(true)
+        HStack(alignment: .top, spacing: 8) {
+            Image(systemName: content.style.iconSystemName)
+                .foregroundColor(content.style.iconColor)
+                .accessibilityHidden(true)
+            VStack(spacing: 4) {
                 Text(content.title)
+                    .bodyMBold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(content.message)
+                    .bodyMRegular()
+                    .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .font(.bundSubtextBold)
-            .foregroundColor(.blackish)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text(content.message)
-                .font(.bundSubtext)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.blackish)
         }
         .padding(16)
         .background(content.style.backgroundColor)
@@ -53,8 +65,16 @@ struct Box: View {
 }
 
 struct InfoBox_Previews: PreviewProvider {
+    
+    static var message = "Body text describing the alert."
+    
     static var previews: some View {
-        Box(content: BoxContent(title: "Info", message: "Message", style: .info))
-        Box(content: BoxContent(title: "Error", message: "Message", style: .error))
+        VStack(spacing: 8) {
+            Box(content: BoxContent(title: "Informational message", message: message, style: .info))
+            Box(content: BoxContent(title: "Warning message", message: message, style: .warning))
+            Box(content: BoxContent(title: "Error message", message: message, style: .error))
+            Box(content: BoxContent(title: "Success message", message: message, style: .success))
+        }
+        .padding()
     }
 }
