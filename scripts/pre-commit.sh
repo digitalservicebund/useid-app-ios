ad88d9cb82d23705787fa5dac5de4f370bbb7577
Andreas Ganske <a.ganske@chaosspace.de>
2023-01-06 15:28:32 +0100

    Specify node version

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 for localizableFile in BundesIdent/Resources/*.lproj/Localizable.strings
 do
     echo "Sorting file - $localizableFile"
     sort "$localizableFile" -o "$localizableFile"
 done
+
+nvm use
 npx git-format-staged --formatter "swiftformat stdin --stdinpath '{}'" "*.swift"
 swiftlint lint --quiet --strict
