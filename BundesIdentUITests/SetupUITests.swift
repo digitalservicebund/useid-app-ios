0b90e37bde69bb81b472ef6ef9d81992a74053b7
Andreas Ganske <andreas.ganske@digitalservice.bund.de>
2023-03-02 09:49:54 +0100

    Add test going through identification with setup inbetween

--- BundesIdentUITests/SetupUITests.swift
+++ BundesIdentUITests/SetupUITests.swift
@@ -43,6 +43,64 @@ final class SetupUITests: XCTestCase {
         app.assertBeingOnHome()
     }
     
+    func testIdentificationWithSetupInbetween() throws {
+        let app = XCUIApplication()
+        app.launchWithDefaultArguments()
+        app.launchWithDemoTokenURL()
+        app.launch()
+        
+        app.buttons[L10n.FirstTimeUser.Intro.startSetup].wait().tap()
+        app.buttons[L10n.FirstTimeUser.PinLetter.letterPresent].wait().tap()
+        
+        let transportPINTextField = app.textFields[L10n.FirstTimeUser.TransportPIN.textFieldLabel]
+        transportPINTextField.wait().tap()
+        transportPINTextField.waitAndTypeText("12345")
+        
+        app.toolbars["Toolbar"].buttons[L10n.FirstTimeUser.TransportPIN.continue].wait().tap()
+        app.buttons[L10n.FirstTimeUser.PersonalPINIntro.continue].wait().tap()
+        
+        let pin1TextField = app.secureTextFields[L10n.FirstTimeUser.PersonalPIN.TextFieldLabel.first]
+        pin1TextField.wait().tap()
+        pin1TextField.waitAndTypeText("123456")
+        
+        app.buttons[L10n.FirstTimeUser.PersonalPIN.continue].wait().tap()
+        
+        let pin2TextField = app.secureTextFields[L10n.FirstTimeUser.PersonalPIN.TextFieldLabel.second]
+        pin2TextField.wait().tap()
+        pin2TextField.waitAndTypeText("123456")
+        app.buttons[L10n.FirstTimeUser.PersonalPIN.continue].wait().tap()
+        
+        app.buttons[L10n.FirstTimeUser.Scan.scan].wait().tap()
+        
+        app.navigationBars.buttons["Debug"].wait().tap()
+        app.buttons["changePINSuccessfully"].wait().tap()
+        
+        app.staticTexts[L10n.FirstTimeUser.Done.title].assertExistence()
+        
+        app.buttons[L10n.FirstTimeUser.Done.identify].wait().tap()
+        
+        app.navigationBars.buttons["Debug"].wait().tap()
+        app.buttons["requestAuthorization"].wait().tap()
+        
+        app.staticTexts[L10n.CardAttribute.dg04].assertExistence()
+        
+        app.buttons[L10n.Identification.AttributeConsent.continue].wait().tap()
+        
+        let pinTextField = app.secureTextFields[L10n.Identification.PersonalPIN.textFieldLabel]
+        pinTextField.wait().tap()
+        pinTextField.waitAndTypeText("123456")
+        
+        app.buttons[L10n.Identification.PersonalPIN.continue].wait().tap()
+        app.navigationBars.buttons["Debug"].wait().tap()
+        app.buttons["identifySuccessfully"].wait().tap()
+        
+        let safari = XCUIApplication(bundleIdentifier: SafariIdentifiers.bundleId.rawValue)
+        XCTAssertEqual(safari.state, .runningForeground)
+        
+        app.activate()
+        app.assertBeingOnHome()
+    }
+    
     func testSetupWrongTransportPINAndEndEarly() throws {
         let app = XCUIApplication()
         app.launchWithDefaultArguments()
