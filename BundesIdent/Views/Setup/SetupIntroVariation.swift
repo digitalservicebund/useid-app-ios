99f449158102a8db3ffa7a014c864019ad4b309a
Daria Kuznetsova <daria.kuznetsova@digitalservice.bund.de>
2023-03-17 13:00:06 +0100

    Modify the view according to the design

--- BundesIdent/Views/Setup/SetupIntroVariation.swift
+++ BundesIdent/Views/Setup/SetupIntroVariation.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import ComposableArchitecture
+import MarkdownUI
 
 struct SetupIntroVariationView: View {
     let store: Store<SetupIntro.State, SetupIntro.Action>
@@ -7,22 +8,41 @@ struct SetupIntroVariationView: View {
     
     var body: some View {
         WithViewStore(store) { viewStore in
-            DialogView(store: store.stateless,
-                       title: "Online-Ausweis noch nicht eingerichtet?",
-                       boxContent: .init(title: "Hinweis", message: "Falls Sie schon über eine persönliche 6-stellige PIN verfügen, ist Ihr Ausweis bereits eingerichtet.", style: .info),
-                       imageMeta: ImageMeta(asset: Asset.eiDs),
-                       secondaryButton: .init(title: L10n.FirstTimeUser.Intro.skipSetup,
-                                              action: .chooseSkipSetup(tokenURL: viewStore.tokenURL)),
-                       primaryButton: .init(title: L10n.FirstTimeUser.Intro.startSetup,
-                                            action: .chooseStartSetup))
-                .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
-                        Button(L10n.General.cancel) {
-                            dismiss()
+            VStack(alignment: .leading, spacing: 0) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 24) {
+                        Text(L10n.FirstTimeUser.Intro.titleVariation)
+                            .headingXL()
+                        VStack(alignment: .center, spacing: 0) {
+                            Markdown(L10n.FirstTimeUser.Intro.box)
+                                .markdownTheme(.bund)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(16)
+                                .foregroundColor(.blackish)
+                            Asset.pinSetupIOS.swiftUIImage
+                                .padding(.bottom, 16)
                         }
-                        .bodyLRegular(color: .accentColor)
+                        .background(Color.blue300)
+                        .cornerRadius(10)
                     }
                 }
+                .padding(.horizontal)
+                DialogButtons(store: store.stateless,
+                              secondary: .init(title: L10n.FirstTimeUser.Intro.skipSetup,
+                                                     action: .chooseSkipSetup(tokenURL: viewStore.tokenURL)),
+                              primary: .init(title: L10n.FirstTimeUser.Intro.startSetup,
+                                                   action: .chooseStartSetup))
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(L10n.General.cancel) {
+                        dismiss()
+                    }
+                    .bodyLRegular(color: .accentColor)
+                }
+            }
+            .ignoresSafeArea(.keyboard)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
@@ -40,5 +60,6 @@ struct SetupIntroVariationView_Previews: PreviewProvider {
                                                  reducer: SetupIntro()))
         }
         .previewDevice("iPhone 12")
+        .environment(\.locale, .init(identifier: "en"))
     }
 }
