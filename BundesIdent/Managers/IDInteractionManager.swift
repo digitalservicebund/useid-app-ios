f04719348962b0e0b9cfa5b7317155b3ccab9fb6
Andreas Ganske <andreas.ganske@digitalservice.bund.de>
2023-04-03 11:48:51 +0200

    Update events

--- BundesIdent/Managers/IDInteractionManager.swift
+++ BundesIdent/Managers/IDInteractionManager.swift
@@ -7,43 +7,27 @@ import OSLog
 import AusweisApp2SDKWrapper
 
 class IDInteractionManager: IDInteractionManagerType {
-    
+
     private let workflowController: WorkflowController
     private let issueTracker: IssueTracker
-    
+
     init(workflowController: WorkflowController = AA2SDKWrapper.workflowController, issueTracker: IssueTracker) {
         self.issueTracker = issueTracker
         self.workflowController = workflowController
     }
-    
+
     func identify(tokenURL: URL, nfcMessagesProvider: NFCConfigType) -> EIDInteractionPublisher {
         start(startServiceHandler: StartServiceHandler(task: .eac(tokenURL: tokenURL)), nfcMessagesProvider: nfcMessagesProvider)
     }
-    
+
     func changePIN(nfcMessagesProvider: NFCConfigType) -> EIDInteractionPublisher {
         start(startServiceHandler: StartServiceHandler(task: .pinManagement), nfcMessagesProvider: nfcMessagesProvider)
     }
-    
+
     private func start(startServiceHandler: StartServiceHandler, nfcMessagesProvider: NFCConfigType) -> EIDInteractionPublisher {
         let publisher = PassthroughSubject<EIDInteractionEvent, IDCardInteractionError>()
         workflowController.start()
         return publisher.eraseToAnyPublisher() // onCancel: cancel workflow?
     }
-    
-    // providePIN(pin: String)
-    
-    // provideNewPIN(newPIN: String)
-    
-    // getCertificate()
-    
-    // provideCAN
-    
-    // providePUK
-    
-    // accept
-    
-    // deny
-    
-    // interrupt
 }
 #endif
